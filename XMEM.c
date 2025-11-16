5d9e6d44b4709d34ff80e8ef919178a560664e64
Kariman Ahmed <35960303+KarimanHelmy@users.noreply.github.com>
2025-11-16 17:35:29 +0200

    Update XMEM.c

--- XMEM.c
+++ XMEM.c
@@ -1,16 +1,12 @@
-/*
- * XMEM.c
- *
- * Created: 15.09.2016 14:12:21
- *  Author: Whiskey Dicks
- */ 
+
 
 #include <avr/io.h>
 #include "bit_macros.h"
-
+//Enable external memory interface, and remove the 4 most significant bits so it doesn't interfere with JTAG
 void XMEM_init(void){
 
-	set_bit(MCUCR,SRE);		//Enable External Memory Interface. PE1 is automatically controlled as this command takes control.
-	set_bit(SFIOR,XMM2);	//Remove 4 Most Significant Bits from address so that JTAG interface doesn't crash
+	set_bit(MCUCR,SRE);		
+	set_bit(SFIOR,XMM2);	
+
 
 }
