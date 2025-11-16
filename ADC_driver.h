764e8df1a72f7c4afc2aca4686c8c422d3b56617
Kariman Ahmed <35960303+KarimanHelmy@users.noreply.github.com>
2025-11-16 17:53:59 +0200

    Update ADC_driver.h

--- ADC_driver.h
+++ ADC_driver.h
@@ -1,9 +1,4 @@
-/*
- * ADC.h
- *
- * Created: 15.09.2016 15:46:43
- *  Author: Whiskey Dicks
- */ 
+
 
 
 #ifndef ADC_H_
@@ -13,13 +8,14 @@ typedef enum {CHANNEL0,CHANNEL1,CHANNEL2,CHANNEL3}channel_t;
 void adc_init (void);
 
 /**
- *      Read analog value from the given channel of the ext. ADC
+ *      Read analog value from a certain channel 
  */
 uint8_t adc_read(uint8_t channel);
 
 /**
- *Read analog values of all channels from the ext. ADC
+ *Read analog value from all chanels
  */
 void adc_read_all(uint8_t * ret_vals);
 
 #endif /* ADC_H_ */
+
